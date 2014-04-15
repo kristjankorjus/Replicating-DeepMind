451266be36c16329c754d073ed4c32194280b9ad
taivop <taivo.pungas@gmail.com>
2014-04-15 15:34:28 +0300

    Basic preprocessing function: resize.

--- src/imageTest.py
+++ src/imageTest.py
@@ -4,7 +4,7 @@ f.close()
 
 print len(pixs)
 
-import Image
+from PIL import Image
 
 img = Image.new('RGB', (160,210), "black")  # create a new black image
 pixels = img.load()  # create the pixel map
@@ -17,3 +17,7 @@ for i in range(len(pixs)/2):
     pixels[row, column] = (int(hex, 16), int(hex, 16), int(hex, 16))
     
 img.show()
+
+from preprocessing import preprocessImage
+
+preprocessImage(img)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,10 +1,8 @@
-def preprocess():
+# input: PIL Image object img, dimensions 210x160
+# output: PIL image object, dimensions 110x84
+def preprocessImage(img):
+    newSize = 110, 84
+    img.thumbnail(newSize) # resizing step
+    img.show()
 
-
-
-
-
-
-
-
-    return
+    return img
