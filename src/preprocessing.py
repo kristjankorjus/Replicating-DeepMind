7b20f75cb49779d3a3f08516c26f63b46c582c58
taivop <taivo.pungas@gmail.com>
2014-04-15 15:57:07 +0300

    Basic preprocessing function: crop and resize. Tailored for Breakout.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,13 @@
 # input: PIL Image object img, dimensions 210x160
 # output: PIL image object, dimensions 110x84
 def preprocessImage(img):
-    newSize = 110, 84
+
+    # crop to 160x160
+    roi = (0, 33, 160, 193) # region of interest (roi) is lines 33 to 193
+    img = img.crop(roi)
+
+    # downscale to 84x84
+    newSize = 84, 84
     img.thumbnail(newSize) # resizing step
     img.show()
 
