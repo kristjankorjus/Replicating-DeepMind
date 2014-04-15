98487956fd8af82bfe17f5c277d66ed1119e9c55
taivop <taivo.pungas@gmail.com>
2014-04-15 16:44:34 +0300

    Sequence preprocessing function. Assuming sequence is a Python list. Tested a bit.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -12,3 +12,14 @@ def preprocessImage(img):
     img.show()
 
     return img
+
+
+# input: list in the form [img1, a1, img2, a2, ...] where img's are PIL Image objects (210x160) and a's are actions (doesn't really matter what kind of objects
+# output: the same list with all the image objects preprocessed
+def preprocessSequence(sequence):
+    from PIL import Image
+    for i in xrange(0,len(sequence),2):             # look at 0-th, 2nd, 4th etc element of the list
+        ob = sequence[i]
+        #assert isinstance(ob, Image)                # make sure Python knows what type of object it has
+        sequence[i] = preprocessImage(ob)           # preprocess and reassign
+    return
