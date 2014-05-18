cf8cd5d07cb82520ebbc5225c7f3865938341fdc
taivop <taivo.pungas@gmail.com>
2014-05-18 17:36:41 +0300

    Chessboard function: can specify board size and square size; plots chessboard without smoothing.

--- src/sandbox/chessboard.py
+++ src/sandbox/chessboard.py
@@ -4,14 +4,16 @@ from math import ceil
 
 
 
-def make_chessboard(n):
+def make_chessboard_any_size(image_size, n):
+    """ Create a image_size x image_size chessboard with square size n pixels.
+    """
     # n is length of small square
 
     n_ones = n * [1]
     n_zeros = n * [0]
 
-    repx = int(ceil(84/(2*n))) + 1
-    print(int(ceil(84/(2*n))))
+    repx = int(ceil(image_size/(2*n))) + 1
+    #print(int(ceil(image_size/(2*n))))
 
     row1 = np.tile(np.array(n_ones + n_zeros),(n,repx))
     row2 = np.tile(np.array(n_zeros + n_ones),(n,repx))
@@ -19,13 +21,17 @@ def make_chessboard(n):
     tworows = np.vstack((row1, row2))
 
     result = np.tile(tworows,(repx,1))
-    result = result[0:84,0:84]
+    result = result[0:image_size,0:image_size]
 
 
     #print('Size: '+ str(np.size(result,0)) + 'x' + str(np.size(result,1)))
     return result
 
 
+def make_chessboard(n):
+    """ Create a 84x84 chessboard with small square size n pixels.
+    """
+    return make_chessboard_any_size(84, n)
 
 
 
@@ -34,8 +40,8 @@ def make_chessboard(n):
 
 def show(result):
     import matplotlib.pyplot as plt
-    plt.imshow(result)
+    plt.imshow(result, interpolation='nearest')
     plt.show()
 
-#arr = make_chessboard(8)
-#show(arr)
+arr = make_chessboard(8)
+show(arr)
