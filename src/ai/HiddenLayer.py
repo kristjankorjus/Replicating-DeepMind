9c09713c9ce3cb12bab518626e1e96db26dd73b3
RDTm <arditampuu@gmail.com>
2014-05-21 17:45:55 +0300

    commented neural network and its sub-classes

--- src/ai/HiddenLayer.py
+++ src/ai/HiddenLayer.py
@@ -1,17 +1,29 @@
+'''
+    Implements hidden layer
+'''
+
+
 import theano
 import numpy as np
 import theano.tensor as T
 
+
 class HiddenLayer:
-    """
-    Implements hidden layer of
-    """
 
     def __init__(self, input, n_in, n_nodes):
+        '''
+        Initialize a hidden layer
+        @param input: theano.tensor.dmatrix of shape (batch_size,n_in), represents inputs from previous layer
+        @param n_in: int, number of inputs to layer
+        @param n_nodes: int, number of nodes in the layer. Also the size of output
+        '''
+
         self.input = input
 
+        #: we need to limit the weight sizes because we might have many inputs to each node
         W_bound = np.sqrt(6. /(n_in+n_nodes))
-        #: Weight matrix (n_in x n_nodes)
+
+        #: Weight matrix (n_in * n_nodes)
         W_values = np.asarray(np.random.uniform(high=W_bound, low=-W_bound, size=(n_in, n_nodes)), dtype=theano.config.floatX)
         self.W = theano.shared(value=W_values, name='W', borrow=True)
 
@@ -19,11 +31,11 @@ class HiddenLayer:
         b_values = np.zeros((n_nodes,), dtype=theano.config.floatX)
         self.b = theano.shared(value=b_values, name='b', borrow=True)
 
-        self.threshold=0
+        self.threshold = 0
         #: Output is rectified
         dot_product = T.dot(input, self.W) + self.b
         above_threshold = dot_product>self.threshold
         self.output = above_threshold * (dot_product-self.threshold)
 
-        #all the variables that can change during learning
+        #: all the variables that can change during learning
         self.params = [self.W, self.b]
--- src/ai/NeuralNet.py
+++ src/ai/NeuralNet.py
@@ -1,9 +1,8 @@
-"""
+'''
 
-NeuralNet class implements neural network and all related stuff
-Probably will be split into several classes
+NeuralNet class creates a Q-learining network by binding together different neural network layers
 
-"""
+'''
 
 from ConvolutionalLayer import *
 from HiddenLayer import *
@@ -13,90 +12,140 @@ theano.config.openmp = True
 
 
 class NeuralNet:
-    def __init__(self, input_shape, filter_shapes, strides, n_hidden, n_out):
 
+    def __init__(self, input_shape, filter_shapes, strides, n_hidden, n_out):
+        '''
+        Initialize a NeuralNet
+
+        @param input_shape: tuple or list of length 4 , (batch size, num input feature maps,
+                             image height, image width)
+        @param filter_shapes: list of 2 (for each conv layer) * 4 values (number of filters, num input feature maps,
+                              filter height,filter width)
+        @param strides: list of size 2, stride values for each hidden layer
+        @param n_hidden: int, number of neurons in the all-to-all connected hidden layer
+        @param n_out: int, number od nudes in output layer
+        '''
+
+        #create theano variables corresponding to input_batch (x) and output of the network (y)
         x = T.dtensor4('x')
         y = T.dmatrix('y')
 
+        #first hidden layer is convolutional:
         self.layer_hidden_conv1 = ConvolutionalLayer(x, filter_shapes[0], input_shape, strides[0])
 
+        #second convolutional hidden layer: the size of input depends on the size of output from first layer
+        #it is defined as (num_batches, num_input_feature_maps, height_of_input_maps, width_of_input_maps)
         second_conv_input_shape = [input_shape[0], filter_shapes[0][0], self.layer_hidden_conv1.feature_map_size,
                                    self.layer_hidden_conv1.feature_map_size]
         self.layer_hidden_conv2 = ConvolutionalLayer(self.layer_hidden_conv1.output, filter_shapes[1],
                                                      image_shape=second_conv_input_shape, stride=2)
 
+        #output from convolutional layer is 4D, but normal hidden layer expects 2D. Because of all to all connections
+        # 3rd hidden layer does not care from which feature map or from which position the input comes from
         flattened_input = self.layer_hidden_conv2.output.flatten(2)
 
+        #create third hidden layer
         self.layer_hidden3 = HiddenLayer(flattened_input, self.layer_hidden_conv2.fan_out, n_hidden)
+
+        #create output layer
         self.layer_output = OutputLayer(self.layer_hidden3.output, n_hidden, n_out)
+
+        #define the ensemble of parameters of the whole network
         self.params = self.layer_hidden_conv1.params + self.layer_hidden_conv2.params \
             + self.layer_hidden3.params + self.layer_output.params
 
+        #discount factor
         self.gamma = 0.95
 
+        #: define regularization terms, for some reason we only take in count the weights, not biases)
+        #  linear regularization term, useful for having many weights zero
         self.l1 = abs(self.layer_hidden_conv1.W).sum() \
             + abs(self.layer_hidden_conv2.W).sum() \
             + abs(self.layer_hidden3.W).sum() \
             + abs(self.layer_output.W).sum()
 
-        # square of L2 norm ; one regularization option is to enforce
-        # square of L2 norm to be small
+        #: square regularization term, useful for forcing small weights
         self.l2_sqr = (self.layer_hidden_conv1.W ** 2).sum() \
             + (self.layer_hidden_conv2.W ** 2).sum() \
             + (self.layer_hidden3.W ** 2).sum() \
             + (self.layer_output.W ** 2).sum()
 
+        #: define the cost function
         cost = 0.0 * self.l1 + 0.0 * self.l2_sqr + self.layer_output.errors(y)
 
+        #: define gradient calculation
         grads = T.grad(cost, self.params)
 
-        # Define how much we need to change the parameter values
+        #: Define how much we need to change the parameter values
         learning_rate = 0.01
         updates = []
         for param_i, gparam_i in zip(self.params, grads):
             updates.append((param_i, param_i - learning_rate * gparam_i))
 
-        temp1 = T.dtensor4('temp1')
-        temp2 = T.dmatrix('temp2')
+        #: we need another set of theano variables (other than x and y) to use in train and predict functions
+        temp_x = T.dtensor4('temp1')
+        temp_y = T.dmatrix('temp2')
 
-        self.train_model = theano.function(inputs=[temp1, temp2],
+        #: define the training operation as applying the updates calculated given temp_x and temp_y
+        self.train_model = theano.function(inputs=[temp_x, temp_y],
                                            outputs=[cost],
                                            updates=updates,
                                            givens={
-                                               x: temp1,
-                                               y: temp2})
+                                               x: temp_x,
+                                               y: temp_y})
 
         self.predict_rewards = theano.function(
-            inputs=[temp1],
+            inputs=[temp_x],
             outputs=[self.layer_output.output],
             givens={
-                x: temp1
+                x: temp_x
             })
 
         self.predict_rewards_and_cost = theano.function(
-            inputs=[temp1, temp2],
+            inputs=[temp_x, temp_y],
             outputs=[self.layer_output.output, cost],
             givens={
-                x: temp1,
-                y: temp2
+                x: temp_x,
+                y: temp_y
             })
 
-    def train(self, minibatch):
-        states = []
-        expected_qs = []
-        states1 = [element['prestate'] for element in minibatch]
-        states2 = [element['poststate'] for element in minibatch]
-        current_predicted_rewards = self.predict_rewards(states1)[0]
 
+    def train(self, minibatch):
+        """
+        Train function that transforms (state,action,reward,state) into (input, expected_output) for neural net
+        and trains the network
+        @param minibatch: array of dictionaries, each dictionary contains
+        one transition (prestate,action,reward,poststate)
+        """
+
+        #: array of size (batch_size,4), that will be filled with our best estimation on what Q-values should be like.
+        #  Corresponds to the "expected_output" or "y" of the neural net
+        expected_qs = []
+
+        initial_states = [element['prestate'] for element in minibatch]
+        states2 = [element['poststate'] for element in minibatch]
+
+        #: our network's current estimation of Q-s for each possible action
+        current_predicted_rewards = self.predict_rewards(initial_states)[0]
+
+        #: the Q-s of the state we actually went to (by choosing an action), we actually only want the maximum of them
         predicted_future_rewards = self.predict_rewards(states2)[0]
+
+        #: we have a new, better estimation for the Q-val of the action we chose, it is the sum of the reward
+        #  received on transition and the maximum of future rewards. Q-s for other actions remain the same.
         for i, transition in enumerate(minibatch):
             rewards = current_predicted_rewards[i]
             rewards[transition['action']] = transition['reward'] + self.gamma * np.max(predicted_future_rewards[i])
-            states.append(transition['prestate'])
             expected_qs.append(rewards)
 
-        self.train_model(states, expected_qs)
+        #: knowing what expected_qs looks like, we can train the model
+        self.train_model(initial_states, expected_qs)
+
 
     def predict_best_action(self, state):
+        """
+        Predict_best_action returns the action with the highest Q-value
+        @param state: 4D array, input (game state) for which we want to know the best action
+        """
         predicted_values_for_actions = self.predict_rewards(state)[0]
         return np.argmax(predicted_values_for_actions)
