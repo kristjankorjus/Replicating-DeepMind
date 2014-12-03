45b8bbcb809d98da8d1fe9f9365aa8e145bd2b4c
NeuroCSUT <neurocsut@gmail.com>
2014-12-03 16:56:33 +0200

    Skeleton for unit testing

---
+++ src/tests/memory/test_memoryD.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from src.memory.memoryd import MemoryD
+
+__author__ = 'deepmind'
+
+
+class TestMemoryD(TestCase):
+
+    def setUp(self):
+        self.memory = MemoryD(10)
+
+    def test_memory_size_at_initialization(self):
+        self.assertTrue(self.memory.size == 10)
