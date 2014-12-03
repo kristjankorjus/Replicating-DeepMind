570127d6b134f4b6ba373c320ecbc74a37d383dc
Tambet Matiisen <tambet.matiisen@gmail.com>
2014-12-03 03:16:34 +0200

    Added logging of training statistics and scores. You can now use command line arguments: <nr_of_epochs> <nr_of_training_frams> <nr_of_testing_frames> Cleaned up code and added some comments.

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from ale.ale import ALE
 import random
 import numpy as np
 import time
-import cPickle
+import sys
 
 class Main:
     # How many transitions to keep in memory?
@@ -25,29 +25,17 @@ class Main:
     # Size of one frame
     frame_size = 84*84
 
-    # How many frames form a history
-    history_length = 4
-
     # Size of one state is four 84x84 screens
-    state_size = history_length * frame_size
+    state_size = 4 * frame_size
 
     # Discount factor for future rewards
     discount_factor = 0.9
 
-    # How many frames to play to choose random frame
-    init_frames = 1000
-    
-    # How many epochs to run
-    epochs = 200
-
-    # Number of frames to play during one training epoch
-    training_frames = 50000
-
-    # Number of frames to play during one testing epoch
-    testing_frames = 10000
-
     # Exploration rate annealing speed
     epsilon_frames = 1000000.0
+
+    # Epsilon during testing
+    test_epsilon = 0.05
 
     # Total frames played, only incremented during training
     total_frames_trained = 0
@@ -81,14 +69,11 @@ class Main:
         return max(1.0 - frames_played / self.epsilon_frames, 0.1)
 
     def predict_best_action(self, last_state):
-        assert last_state.shape[0] == self.state_size
-        assert len(last_state.shape) == 1
-
         # last_state contains only one state, so we have to convert it into batch of size 1
         last_state.shape = (last_state.shape[0], 1)
+
+        # use neural net to predict Q-values for all actions
         qvalues = self.nnet.predict(last_state)
-        assert qvalues.shape[0] == 1
-        assert qvalues.shape[1] == self.number_of_actions
         #print "Predicted action Q-values: ", qvalues
 
         # return action (index) with maximum Q-value
@@ -100,32 +85,14 @@ class Main:
         and trains the network
         @param minibatch: list of arrays: prestates, actions, rewards, poststates
         """
-        prestates = minibatch[0]
-        actions = minibatch[1]
-        rewards = minibatch[2]
-        poststates = minibatch[3]
-
-        assert prestates.shape[0] == self.state_size
-        assert prestates.shape[1] == self.minibatch_size
-        assert poststates.shape[0] == self.state_size
-        assert poststates.shape[1] == self.minibatch_size
-        assert actions.shape[0] == self.minibatch_size
-        assert rewards.shape[0] == self.minibatch_size
-
-        # predict Q-values for poststates
-        post_qvalues = self.nnet.predict(poststates)
-        assert post_qvalues.shape[0] == self.minibatch_size
-        assert post_qvalues.shape[1] == self.number_of_actions
-
-        # take maximum Q-value of all actions
-        max_qvalues = np.max(post_qvalues, axis=1)
-        assert max_qvalues.shape[0] == self.minibatch_size
-        assert len(max_qvalues.shape) == 1
+        prestates, actions, rewards, poststates = minibatch
 
         # predict Q-values for prestates, so we can keep Q-values for other actions unchanged
         qvalues = self.nnet.predict(prestates)
-        assert qvalues.shape[0] == self.minibatch_size
-        assert qvalues.shape[1] == self.number_of_actions
+        # predict Q-values for poststates
+        post_qvalues = self.nnet.predict(poststates)
+        # take maximum Q-value of all actions
+        max_qvalues = np.max(post_qvalues, axis = 1)
 
         # update the Q-values for the actions we actually performed
         for i, action in enumerate(actions):
@@ -135,13 +102,14 @@ class Main:
         cost = self.nnet.train(prestates, qvalues.transpose().copy())
         return cost
 
-    def play_games(self, nr_frames, train, epsilon):
+    def play_games(self, nr_frames, train, epsilon = None):
         """
         Main cycle: starts a game and plays number of frames.
         @param nr_frames: total number of games allowed to play
         @param train: true or false, whether to do training or not
         @param epsilon: fixed epsilon, only used when not training
         """
+        assert train or epsilon is not None
 
         frames_played = 0
         game_scores = []
@@ -170,27 +138,27 @@ class Main:
             # Make the move
             reward = self.ale.move(action)
             if reward:
-                print "    Got reward of %d!!!" % reward
                 reward = 1
+                print "    Got reward of %d" % reward
             game_score += reward
             frames_played += 1
             #print "Played frame %d" % frames_played
 
-            # Store new information to memory
-            self.ale.store_step(action)
-
             # Only if training
             if train:
+                # Store new information to memory
+                self.ale.store_step(action)
                 # Increase total frames only when training
                 self.total_frames_trained += 1
-                # Train neural net with random minibatch
+                # Fetch random minibatch from memory
                 minibatch = self.memory.get_minibatch(self.minibatch_size)
+                # Train neural net with the minibatch
                 self.train_minibatch(minibatch)
                 #print "Trained minibatch of size %d" % self.minibatch_size
 
             # Play until game is over
             if self.ale.game_over:
-                print "   Game over!!! Score = %d" % game_score
+                print "    Game over, score = %d" % game_score
                 # After "game over" increase the number of games played
                 game_scores.append(game_score);
                 game_score = 0
@@ -203,46 +171,72 @@ class Main:
 
         return game_scores
 
-    def run(self):
-        # Play number of random games and pick random states to calculate Q-values for
-        print "Playing %d games with random policy" % self.init_frames
-        self.play_games(self.init_frames, False, 1)
-        self.random_states = self.memory.get_minibatch(self.nr_random_states)[0]
+    def run(self, epochs, training_frames, testing_frames):
+        # Open log files and write headers
+        timestamp = time.strftime("%Y-%m-%d-%H-%M")
+        log_train = open("../log/training_" + timestamp + ".csv", "w")
+        log_train.write("epoch,nr_games,sum_score,average_score,nr_frames,total_frames_trained,epsilon,memory_size\n")
+        log_test = open("../log/testing_" + timestamp + ".csv", "w")
+        log_test.write("epoch,nr_games,sum_score,average_score,average_qvalue,nr_frames,epsilon,memory_size\n")
+        log_train_scores = open("../log/training_scores_" + timestamp + ".txt", "w")
+        log_test_scores = open("../log/testing_scores_" + timestamp + ".txt", "w")
 
-        # Open log file and write header
-        log_file = open("../log/scores" + time.strftime("%Y-%m-%d-%H-%M") + ".csv", "w")
-        log_file.write("epoch,nr_games,sum_score,average_score,nr_frames_tested,average_qvalue,total_frames_trained,epsilon,memory_size\n")
-
-        for epoch in range(1, self.epochs + 1):
+        for epoch in range(1, epochs + 1):
             print "Epoch %d:" % epoch
             # play number of frames with training and epsilon annealing
-            print "  Training for %d frames" % self.training_frames
-            self.play_games(self.training_frames, True, None)
+            print "  Training for %d frames" % training_frames
+            training_scores = self.play_games(training_frames, train = True)
+
+            # log training scores
+            log_train_scores.write('\n'.join(map(str, training_scores)) + '\n')
+            log_train_scores.flush()
+
+            # log aggregated training data
+            train_data = (epoch, len(training_scores), sum(training_scores), np.mean(training_scores), training_frames, self.total_frames_trained, self.compute_epsilon(self.total_frames_trained), self.memory.count)
+            log_train.write(','.join(map(str, train_data)) + '\n')
+            log_train.flush()
+
             # play number of frames without training and without epsilon annealing
-            print "  Testing for %d frames" % self.testing_frames
-            game_scores = self.play_games(self.testing_frames, False, 0.05)
+            print "  Testing for %d frames" % testing_frames
+            testing_scores = self.play_games(testing_frames, train = False, epsilon = self.test_epsilon)
+
+            # log testing scores
+            log_test_scores.write('\n'.join(map(str, testing_scores)) + '\n')
+            log_test_scores.flush()
+
+            # Pick random states to calculate Q-values for
+            if self.random_states is None:
+                print "  Picking %d random states for Q-values" % self.nr_random_states
+                self.random_states = self.memory.get_minibatch(self.nr_random_states)[0]
 
             # calculate Q-values 
             qvalues = self.nnet.predict(self.random_states)
             assert qvalues.shape[0] == self.nr_random_states
             assert qvalues.shape[1] == self.number_of_actions
-            max_qvalues = np.max(qvalues, axis=1)
+            max_qvalues = np.max(qvalues, axis = 1)
             assert max_qvalues.shape[0] == self.nr_random_states
             assert len(max_qvalues.shape) == 1
             avg_qvalue = np.mean(max_qvalues)
 
-            # calculate average scores
-            sum_score = sum(game_scores)
-            nr_games = len(game_scores)
-            avg_score = np.mean(game_scores)
-            epsilon = self.compute_epsilon(self.total_frames_trained)
-            
-            # log average scores in file
-            log_file.write("%d,%d,%f,%f,%d,%f,%d,%f,%d\n" % (epoch, nr_games, sum_score, avg_score, self.testing_frames, avg_qvalue, self.total_frames_trained, epsilon, self.memory.count))
-            log_file.flush()
+            # log aggregated testing data
+            test_data = (epoch, len(testing_scores), sum(testing_scores), np.mean(testing_scores), avg_qvalue, testing_frames, self.test_epsilon, self.memory.count)
+            log_test.write(','.join(map(str, test_data)) + '\n')
+            log_test.flush()
 
-        log_file.close()
+        log_train.close()
+        log_test.close()
 
 if __name__ == '__main__':
+    # ignore cuda-convnet options at start of command line
+    i = 1
+    while (i < len(sys.argv) and sys.argv[i].startswith('--')):
+        i += 2
+
+    # take some parameters from command line, otherwise use defaults
+    epochs = int(sys.argv[i]) if len(sys.argv) > i else 100
+    training_frames = int(sys.argv[i + 1]) if len(sys.argv) > i + 1 else 50000
+    testing_frames = int(sys.argv[i + 2]) if len(sys.argv) > i + 2 else 10000
+
+    # run the main loop
     m = Main()
-    m.run()
+    m.run(epochs, training_frames, testing_frames)
