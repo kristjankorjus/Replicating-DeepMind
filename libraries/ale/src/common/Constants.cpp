b2b9413021bf2da62d85f9e359df722d775a6df5
Ilya Kuzovkin <ilya.kuzovkin@gmail.com>
2014-12-17 17:28:17 +0200

    Useful comments :)

--- libraries/ale/src/common/Constants.cpp
+++ libraries/ale/src/common/Constants.cpp
@@ -17,42 +17,42 @@
 
 std::string action_to_string(Action a) {
     static string tmp_action_to_string[] = {
-        "PLAYER_A_NOOP"          
-        ,"PLAYER_A_FIRE"          
-        ,"PLAYER_A_UP"            
-        ,"PLAYER_A_RIGHT"         
-        ,"PLAYER_A_LEFT"          
-        ,"PLAYER_A_DOWN"          
-        ,"PLAYER_A_UPRIGHT"       
-        ,"PLAYER_A_UPLEFT"        
-        ,"PLAYER_A_DOWNRIGHT"     
-        ,"PLAYER_A_DOWNLEFT"      
-        ,"PLAYER_A_UPFIRE"        
-        ,"PLAYER_A_RIGHTFIRE"     
-        ,"PLAYER_A_LEFTFIRE"      
-        ,"PLAYER_A_DOWNFIRE"      
-        ,"PLAYER_A_UPRIGHTFIRE"   
-        ,"PLAYER_A_UPLEFTFIRE"    
-        ,"PLAYER_A_DOWNRIGHTFIRE"
-        ,"PLAYER_A_DOWNLEFTFIRE"
-        ,"PLAYER_B_NOOP"          
-        ,"PLAYER_B_FIRE"          
-        ,"PLAYER_B_UP"            
-        ,"PLAYER_B_RIGHT"         
-        ,"PLAYER_B_LEFT"          
-        ,"PLAYER_B_DOWN"          
-        ,"PLAYER_B_UPRIGHT"       
-        ,"PLAYER_B_UPLEFT"        
-        ,"PLAYER_B_DOWNRIGHT"     
-        ,"PLAYER_B_DOWNLEFT"      
-        ,"PLAYER_B_UPFIRE"        
-        ,"PLAYER_B_RIGHTFIRE"     
-        ,"PLAYER_B_LEFTFIRE"      
-        ,"PLAYER_B_DOWNFIRE"      
-        ,"PLAYER_B_UPRIGHTFIRE"   
-        ,"PLAYER_B_UPLEFTFIRE"    
-        ,"PLAYER_B_DOWNRIGHTFIRE"
-        ,"PLAYER_B_DOWNLEFTFIRE"
+        "PLAYER_A_NOOP"           //  0
+        ,"PLAYER_A_FIRE"          //  1
+        ,"PLAYER_A_UP"            //  2
+        ,"PLAYER_A_RIGHT"         //  3
+        ,"PLAYER_A_LEFT"          //  4
+        ,"PLAYER_A_DOWN"          //  5
+        ,"PLAYER_A_UPRIGHT"       //  6
+        ,"PLAYER_A_UPLEFT"        //  7
+        ,"PLAYER_A_DOWNRIGHT"     //  8
+        ,"PLAYER_A_DOWNLEFT"      //  9
+        ,"PLAYER_A_UPFIRE"        // 10
+        ,"PLAYER_A_RIGHTFIRE"     // 11
+        ,"PLAYER_A_LEFTFIRE"      // 12
+        ,"PLAYER_A_DOWNFIRE"      // 13
+        ,"PLAYER_A_UPRIGHTFIRE"   // 14
+        ,"PLAYER_A_UPLEFTFIRE"    // 15
+        ,"PLAYER_A_DOWNRIGHTFIRE" // 16
+        ,"PLAYER_A_DOWNLEFTFIRE"  // 17
+        ,"PLAYER_B_NOOP"          // 18
+        ,"PLAYER_B_FIRE"          // 19
+        ,"PLAYER_B_UP"            // 20
+        ,"PLAYER_B_RIGHT"         // 21
+        ,"PLAYER_B_LEFT"          // 22
+        ,"PLAYER_B_DOWN"          // 23
+        ,"PLAYER_B_UPRIGHT"       // 24
+        ,"PLAYER_B_UPLEFT"        // 25
+        ,"PLAYER_B_DOWNRIGHT"     // 26
+        ,"PLAYER_B_DOWNLEFT"      // 27
+        ,"PLAYER_B_UPFIRE"        // 28
+        ,"PLAYER_B_RIGHTFIRE"     // 29
+        ,"PLAYER_B_LEFTFIRE"      // 30
+        ,"PLAYER_B_DOWNFIRE"      // 31
+        ,"PLAYER_B_UPRIGHTFIRE"   // 32
+        ,"PLAYER_B_UPLEFTFIRE"    // 33
+        ,"PLAYER_B_DOWNRIGHTFIRE" // 34
+        ,"PLAYER_B_DOWNLEFTFIRE"  // 35
         ,"__invalid__" // 36
         ,"__invalid__" // 37
         ,"__invalid__" // 38
